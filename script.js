78651ee90b40fea16b7cf3d06ce67a4e0940377d
saphalthedeveloper-cyber <saphalthedeveloper@gmail.com>
2026-01-06 19:01:25 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputBox=document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value===''){
+        alert("you have to write something");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value='';
+    
+    saveData();
+
+}
+listContainer.addEventListener("click",(e)=>{
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showList(){
+   let data=listContainer.innerHTML=localStorage.getItem("data");
+    if(data !== "undefined" && data !== "null"){
+        listContainer.innerHTML=data;
+    }else{
+        listContainer.innerHTML="";
+    }
+}
+showList();
+
+
